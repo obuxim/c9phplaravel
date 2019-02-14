2337db1ab0cc263f84e6a3adb397ec7c019970e2
Zubair Hasan Zubo <obuxim@gmail.com>
2019-02-14 18:27:38 +0600

    Update to PHP 7.3
    - Updated to PHP7.3 from PHP7.1
    - Fixed Composer Corrupted Issue

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
     sudo apt-add-repository ppa:ondrej/php &&
     sudo apt-get update -y &&
-    sudo apt-get install -y php7.1 php7.1-common php7.1-mysql php7.1-pgsql php7.1-mbstring php7.1-mcrypt php7.1-xml php7.1-zip php7.1-gd &&
+    sudo apt-get install -y php7.3 php7.3-common php7.3-mysql php7.3-pgsql php7.3-mbstring php7.3-xml php7.3-zip php7.3-gd &&
     sudo a2dismod php5 &&
-    sudo a2enmod php7.1 &&
+    sudo a2enmod php7.3 &&
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php -r "if (hash_file('SHA384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     php composer-setup.php
     php -r "unlink('composer-setup.php');"  &&
     sudo mv composer.phar /usr/local/bin/composer &&
